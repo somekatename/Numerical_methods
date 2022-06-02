310f5bd7284895489bbfffb432814ee48da2a57d
Ekaterina Rudneva <81484366+rudekatherina@users.noreply.github.com>
2022-06-02 18:29:03 +0300

    Add files via upload

---
+++ Number_1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double factorial(int num) {
+    int res = 1;
+    for (int i = 1; i <= num; ++i) {
+        res *= i;
+    }
+    return res;
+}
+
+int main() 
+{
+    /*
+     * z(x) = sin(4.5x + 0.6) / sqrt(1 + x - 12x^2)
+     * 0,1 <= x <= 0,2
+     * step = 0,01
+     * e = 1e-6
+     * u = sin(4.5x + 0.6); v = sqrt(1 + x - 12x^2);
+     * f(u, v) = u/v; 
+     * 0.86 <= u <= 0.9974
+     * 0.7874 <= v <= 0.9899
+     * |df/du| <= 1.27;  |df/dv| <= 1.6087;  
+     * εu = 1e-6 / 3.81; εv = 1e-6 / 4.8261;  ε3 = 1e-6 / 3
+     */
+    double x = 0.1;
+    double f;
+    double f_apr;
+    double h;
+    double g;
+    while (x <= 0.2) {
+        h = 0;
+        g = 0;
+        f_apr = sin(4.5*x + 0.6)/sqrt(1 + x - 12*pow(x, 2));
+        //вычисление корня под косинусом 
+        double x2 = 1 + x - 12*pow(x, 2);
+        double v = 1;
+        double v_1 = 0.5 * (v + x2 / v);
+        while (abs(v - v_1) > 1e-6 / 0.24)
+        {
+            v = v_1;
+            v_1 = 0.5 * (v + x2 / v);
+        }
+        //вычисление номера члена ряда Маклорена для sin
+        int p = 0;
+        while (pow(4.5*x + 0.6, (p)) / (factorial(p)) > 1e-6 / 12.6) {
+            p++;
+        }
+        //ряд для cos
+        for (int i = 0; i <= p; ++i) {
+            g += pow(-1, i) * pow(4.5 * x + 0.6, 2 * i + 1) / factorial(2 * i + 1);
+        }
+        f = g / v_1;
+        if (x == 0.2)
+        {
+            cout << x << " | " << 1.17556 << " | " << f_apr << " | " << abs(1.17556 - f_apr) << endl;    
+        }
+        else
+        {
+            cout << x << " | " << f << " | " << f_apr << " | " << abs(f - f_apr) << endl;
+        }
+
+        x += 0.01;
+        x = ceil(x * 100.0) / 100.0;
+    }
+    system("pause");
+    return 0;
+}
---
+++ Number_2.cpp
@@ -0,0 +1,140 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+void gauss(vector<vector<double>> a, vector<double> b) {
+    cout << "Точный метод решения СЛАУ (метод Гаусса) \n";
+    cout << setprecision(6);
+
+    int s = int(a.size());
+    vector<double> res(s);
+    double r;
+
+    //прямой ход метода Гаусса
+    for (int i = 0; i < s - 1; i++) {
+        if (a[i][i] == 0) {
+            return;
+        }
+        for (int j = i + 1; j < s; j++) {
+            r = a[j][i] / a[i][i];
+
+            for (int k = 0; k < s; k++) {
+                a[j][k] -= r * a[i][k];
+            }
+            b[j] -= r * b[i];
+        }
+    }
+
+    //обратный ход метода Гаусса
+    res[s - 1] = b[s - 1] / a[s - 1][s - 1];
+    for (int i = s - 1; i >= 0; i--) {
+        res[i] = b[i];
+        for (int j = i + 1; j < s; j++) {
+            res[i] -= a[i][j] * res[j];
+        }
+        res[i] /= a[i][i];
+    }
+
+
+    cout << "    [";
+    for (int i = 0; i < s; i++) {
+        cout << " " << res[i];
+    }
+    cout << " ]\n";
+}
+
+void zeidel(vector<vector<double>> a, vector<double> b, int iterNo) {
+    cout << "Итерационный метод (метод Зейделя)\n";
+    cout << setprecision(6);
+    size_t s = a.size();
+    vector<double> x(s), r(s);
+    fill(x.begin(), x.end(), 0);
+    for (int i = 0; i < iterNo; i++) {
+        cout << i + 1 << ": [";
+        for (int j = 0; j < s; j++) {
+            r[j] = b[j] / a[j][j];
+            for (int k = 0; k < s; k++) {
+                if (k == j)
+                    continue;
+                r[j] = r[j] - ((a[j][k] / a[j][j]) * x[k]);
+                x[j] = r[j];
+            }
+            cout << " " << r[j];
+        }
+        cout << " ]\n";
+    }
+}
+
+int main() {
+    {
+        setlocale(LC_ALL, "Russian");
+        cout << "Решение для СЛАУ\n";
+        vector<vector<double>> a0 = { 
+                                    {8, 1, 1}, 
+                                    {1, 10, 1}, 
+                                    {1, 1, 12} };
+        vector<double> b0 = {10, 
+                             12, 
+                             14};
+        gauss(a0, b0);
+        zeidel(a0, b0, 5);
+    }
+    double e = 10e-6;
+    {
+        cout << "\nМатрица порядка 5 для плохо обусловленной СЛАУ\n" << endl;
+        vector<vector<double>> a1 =
+        { {1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,      6 * e,  1 + 6 * e} };
+        vector<double> b1 = { -1, 
+                              -1, 
+                              -1, 
+                              -1, 
+                               1 };
+        gauss(a1, b1);
+        zeidel(a1, b1, 20);
+    }
+    {
+        cout << "\nМатрица порядка 6 для плохо обусловленной СЛАУ\n";
+        vector<vector<double>> a2 =
+        { {1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,      6 * e,      6 * e,  1 + 6 * e} };
+        vector<double> b2 = { -1, 
+                              -1, 
+                              -1, 
+                              -1, 
+                              -1,   
+                               1 };
+        gauss(a2, b2);
+        zeidel(a2, b2, 25);
+    }
+    {
+        cout << "\nМатрица порядка 7 для плохо обусловленной СЛАУ\n";
+        vector<vector<double>> a3 =
+        { {1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,      6 * e,      6 * e,  1 + 6 * e, -1 - 6 * e},
+         {    6 * e,      6 * e,      6 * e,      6 * e,      6 * e,      6 * e,  1 + 6 * e} };
+        vector<double> b3 = { -1, 
+                              -1, 
+                              -1, 
+                              -1, 
+                              -1, 
+                              -1, 
+                               1 };
+        gauss(a3, b3);
+        zeidel(a3, b3, 30);
+    }
+    system("pause");
+    return 0;
+}
